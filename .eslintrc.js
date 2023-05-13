6eb804794c323525c762a50b03f48f66ab495d0e
Chiara Campalani <campalanichiara@gmail.com>
2023-05-13 18:52:04 +0900

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    '@react-native-community'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'never'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-use-before-define': [
+      'error',
+      { functions: true, variables: false, classes: false }
+    ],
+    'prettier/prettier': 'error'
+  }
+}
