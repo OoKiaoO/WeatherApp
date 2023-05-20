cb4e7b5789e9c3fac555768128288b801e1fe35b
Chiara Campalani <campalanichiara@gmail.com>
2023-05-20 13:25:56 +0900

    weather api connection setup

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ActivityIndicator, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/components/Tabs';
+import * as Location from 'expo-location';
+import { WEATHER_API_KEY } from "@env"; // ALWAYS KEEP DOUBLE QUOTES FORF THIS IMPORT!!!
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setError('permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location);
+    })() //since we want to call it immediately we pass a set of parentheses at the end
+  }, []) // we pass an empty array into useEffect to mean there are no dependencies and useEffect will only run once after is rendered
+
+  if (location) {
+    console.log(location);
+  }
+  console.log('hello');
   if (loading) {
     return (
       <View style={styles.container}>
@@ -13,6 +35,7 @@ const App = () => {
       </View>
     )
   }
+
   return (
     <NavigationContainer>
       <Tabs />
